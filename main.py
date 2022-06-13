a16e4d15a0c290bb74c4f70866dfb004ef4e0505
Igor <Aleshichev.git@email.ua>
2022-06-13 16:27:54 +0300

    2 method GET jsonify

--- main.py
+++ main.py
@@ -24,6 +24,19 @@ class Cafe(db.Model):
     can_take_calls = db.Column(db.Boolean, nullable=False)
     coffee_price = db.Column(db.String(250), nullable=True)
 
+    def to_dict(self):
+        # Method 1.
+        dictionary = {}
+        # Loop through each column in the data record
+        for column in self.__table__.columns:
+            # Create a new dictionary entry;
+            # where the key is the name of the column
+            # and the value is the value of the column
+            dictionary[column.name] = getattr(self, column.name)
+        return dictionary
+
+    # Method 2. Altenatively use Dictionary Comprehension to do the same thing.
+    # return {column.name: getattr(self, column.name) for column in self.__table__.columns}
 
 @app.route("/")
 def home():
@@ -35,19 +48,21 @@ def home():
 def get_random_cafe():
     cafes = db.session.query(Cafe).all()
     random_cafe = random.choice(cafes)
-    return jsonify(cafe={
-        "id": random_cafe.id,
-        "name": random_cafe.name,
-        "map_url": random_cafe.map_url,
-        "img_url": random_cafe.img_url,
-        "location": random_cafe.location,
-        "seats": random_cafe.seats,
-        "has_toilet": random_cafe.has_toilet,
-        "has_wifi": random_cafe.has_wifi,
-        "has_sockets": random_cafe.has_sockets,
-        "can_take_calls": random_cafe.can_take_calls,
-        "coffee_price": random_cafe.coffee_price,
-    })
+    return jsonify(cafe=random_cafe.to_dict())
+
+    # return jsonify(cafe={
+    #     "id": random_cafe.id,
+    #     "name": random_cafe.name,
+    #     "map_url": random_cafe.map_url,
+    #     "img_url": random_cafe.img_url,
+    #     "location": random_cafe.location,
+    #     "seats": random_cafe.seats,
+    #     "has_toilet": random_cafe.has_toilet,
+    #     "has_wifi": random_cafe.has_wifi,
+    #     "has_sockets": random_cafe.has_sockets,
+    #     "can_take_calls": random_cafe.can_take_calls,
+    #     "coffee_price": random_cafe.coffee_price,
+    # })
 
 ## HTTP POST - Create Record
 
